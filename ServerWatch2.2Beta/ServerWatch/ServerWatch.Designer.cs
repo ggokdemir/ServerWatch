4feb4e510095dcaeb0c2770f2381fe3cf0fc9e0b
Gürkan Gökdemir <25768163+gurkangokdemir@users.noreply.github.com>
2019-12-05 14:40:28 +0300

    Add files via upload

---
+++ ServerWatch2.2Beta/ServerWatch/ServerWatch.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace ServerWatch
+{
+    partial class ServerWatch
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.labelMainServer = new System.Windows.Forms.Label();
+            this.textBoxMainServer = new System.Windows.Forms.TextBox();
+            this.labelServer1 = new System.Windows.Forms.Label();
+            this.textBoxServer1 = new System.Windows.Forms.TextBox();
+            this.labelServer2 = new System.Windows.Forms.Label();
+            this.textBoxServer2 = new System.Windows.Forms.TextBox();
+            this.labelServer3 = new System.Windows.Forms.Label();
+            this.textBoxServer3 = new System.Windows.Forms.TextBox();
+            this.labelServer4 = new System.Windows.Forms.Label();
+            this.textBoxServer4 = new System.Windows.Forms.TextBox();
+            this.labelServer5 = new System.Windows.Forms.Label();
+            this.textBoxServer5 = new System.Windows.Forms.TextBox();
+            this.textBoxBanner = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(15, 12);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(312, 44);
+            this.buttonStart.TabIndex = 0;
+            this.buttonStart.Text = "START";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // labelMainServer
+            // 
+            this.labelMainServer.AutoSize = true;
+            this.labelMainServer.Location = new System.Drawing.Point(12, 90);
+            this.labelMainServer.Name = "labelMainServer";
+            this.labelMainServer.Size = new System.Drawing.Size(80, 17);
+            this.labelMainServer.TabIndex = 2;
+            this.labelMainServer.Text = "MainServer";
+            // 
+            // textBoxMainServer
+            // 
+            this.textBoxMainServer.Location = new System.Drawing.Point(104, 90);
+            this.textBoxMainServer.Name = "textBoxMainServer";
+            this.textBoxMainServer.Size = new System.Drawing.Size(223, 22);
+            this.textBoxMainServer.TabIndex = 6;
+            // 
+            // labelServer1
+            // 
+            this.labelServer1.AutoSize = true;
+            this.labelServer1.Location = new System.Drawing.Point(12, 118);
+            this.labelServer1.Name = "labelServer1";
+            this.labelServer1.Size = new System.Drawing.Size(58, 17);
+            this.labelServer1.TabIndex = 2;
+            this.labelServer1.Text = "Server1";
+            // 
+            // textBoxServer1
+            // 
+            this.textBoxServer1.Location = new System.Drawing.Point(104, 118);
+            this.textBoxServer1.Name = "textBoxServer1";
+            this.textBoxServer1.Size = new System.Drawing.Size(223, 22);
+            this.textBoxServer1.TabIndex = 6;
+            // 
+            // labelServer2
+            // 
+            this.labelServer2.AutoSize = true;
+            this.labelServer2.Location = new System.Drawing.Point(12, 146);
+            this.labelServer2.Name = "labelServer2";
+            this.labelServer2.Size = new System.Drawing.Size(58, 17);
+            this.labelServer2.TabIndex = 2;
+            this.labelServer2.Text = "Server2";
+            // 
+            // textBoxServer2
+            // 
+            this.textBoxServer2.Location = new System.Drawing.Point(104, 146);
+            this.textBoxServer2.Name = "textBoxServer2";
+            this.textBoxServer2.Size = new System.Drawing.Size(223, 22);
+            this.textBoxServer2.TabIndex = 6;
+            // 
+            // labelServer3
+            // 
+            this.labelServer3.AutoSize = true;
+            this.labelServer3.Location = new System.Drawing.Point(12, 174);
+            this.labelServer3.Name = "labelServer3";
+            this.labelServer3.Size = new System.Drawing.Size(58, 17);
+            this.labelServer3.TabIndex = 2;
+            this.labelServer3.Text = "Server3";
+            // 
+            // textBoxServer3
+            // 
+            this.textBoxServer3.Location = new System.Drawing.Point(104, 174);
+            this.textBoxServer3.Name = "textBoxServer3";
+            this.textBoxServer3.Size = new System.Drawing.Size(223, 22);
+            this.textBoxServer3.TabIndex = 6;
+            // 
+            // labelServer4
+            // 
+            this.labelServer4.AutoSize = true;
+            this.labelServer4.Location = new System.Drawing.Point(12, 202);
+            this.labelServer4.Name = "labelServer4";
+            this.labelServer4.Size = new System.Drawing.Size(58, 17);
+            this.labelServer4.TabIndex = 2;
+            this.labelServer4.Text = "Server4";
+            // 
+            // textBoxServer4
+            // 
+            this.textBoxServer4.Location = new System.Drawing.Point(104, 202);
+            this.textBoxServer4.Name = "textBoxServer4";
+            this.textBoxServer4.Size = new System.Drawing.Size(223, 22);
+            this.textBoxServer4.TabIndex = 6;
+            // 
+            // labelServer5
+            // 
+            this.labelServer5.AutoSize = true;
+            this.labelServer5.Location = new System.Drawing.Point(12, 230);
+            this.labelServer5.Name = "labelServer5";
+            this.labelServer5.Size = new System.Drawing.Size(58, 17);
+            this.labelServer5.TabIndex = 2;
+            this.labelServer5.Text = "Server5";
+            // 
+            // textBoxServer5
+            // 
+            this.textBoxServer5.Location = new System.Drawing.Point(104, 230);
+            this.textBoxServer5.Name = "textBoxServer5";
+            this.textBoxServer5.Size = new System.Drawing.Size(223, 22);
+            this.textBoxServer5.TabIndex = 6;
+            // 
+            // textBoxBanner
+            // 
+            this.textBoxBanner.Location = new System.Drawing.Point(12, 62);
+            this.textBoxBanner.Name = "textBoxBanner";
+            this.textBoxBanner.Size = new System.Drawing.Size(312, 22);
+            this.textBoxBanner.TabIndex = 6;
+            // 
+            // ServerWatch
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(343, 276);
+            this.Controls.Add(this.textBoxServer5);
+            this.Controls.Add(this.labelServer5);
+            this.Controls.Add(this.textBoxServer4);
+            this.Controls.Add(this.labelServer4);
+            this.Controls.Add(this.textBoxServer3);
+            this.Controls.Add(this.labelServer3);
+            this.Controls.Add(this.textBoxServer2);
+            this.Controls.Add(this.labelServer2);
+            this.Controls.Add(this.textBoxServer1);
+            this.Controls.Add(this.labelServer1);
+            this.Controls.Add(this.textBoxBanner);
+            this.Controls.Add(this.textBoxMainServer);
+            this.Controls.Add(this.labelMainServer);
+            this.Controls.Add(this.buttonStart);
+            this.Name = "ServerWatch";
+            this.Text = "ServerWatch";
+            this.Load += new System.EventHandler(this.ServerWatch_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.Button buttonStart;
+        public System.Windows.Forms.Label labelMainServer;
+        public System.Windows.Forms.TextBox textBoxMainServer;
+        public System.Windows.Forms.Label labelServer1;
+        public System.Windows.Forms.TextBox textBoxServer1;
+        public System.Windows.Forms.Label labelServer2;
+        public System.Windows.Forms.TextBox textBoxServer2;
+        public System.Windows.Forms.Label labelServer3;
+        public System.Windows.Forms.TextBox textBoxServer3;
+        public System.Windows.Forms.Label labelServer4;
+        public System.Windows.Forms.TextBox textBoxServer4;
+        public System.Windows.Forms.Label labelServer5;
+        public System.Windows.Forms.TextBox textBoxServer5;
+        public System.Windows.Forms.TextBox textBoxBanner;
+    }
+}
+
