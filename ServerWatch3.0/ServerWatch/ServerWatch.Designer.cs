c2b0e10861dfedd23b43f034374673e732b76102
Gürkan Gökdemir <25768163+gurkangokdemir@users.noreply.github.com>
2019-12-06 23:17:17 +0300

    Add files via upload

---
+++ ServerWatch3.0/ServerWatch/ServerWatch.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace ServerWatch
+{
+    partial class ServerWatch
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.textBoxBanner = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(12, 12);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(437, 44);
+            this.buttonStart.TabIndex = 0;
+            this.buttonStart.Text = "START";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // textBoxBanner
+            // 
+            this.textBoxBanner.Location = new System.Drawing.Point(12, 62);
+            this.textBoxBanner.Name = "textBoxBanner";
+            this.textBoxBanner.Size = new System.Drawing.Size(437, 22);
+            this.textBoxBanner.TabIndex = 6;
+            // 
+            // ServerWatch
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(461, 93);
+            this.Controls.Add(this.textBoxBanner);
+            this.Controls.Add(this.buttonStart);
+            this.Name = "ServerWatch";
+            this.Text = "ServerWatch";
+            this.Load += new System.EventHandler(this.ServerWatch_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.Button buttonStart;
+        public System.Windows.Forms.TextBox textBoxBanner;
+    }
+}
+
